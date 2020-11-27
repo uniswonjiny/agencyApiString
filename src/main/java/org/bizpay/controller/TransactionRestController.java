349902d85fa377453b272031bf2b996847c57dee
uni.swonjiny <uni.swonjiny@gmail.com>
2020-11-27 14:15:24 +0900

    20201127

--- src/main/java/org/bizpay/controller/TransactionRestController.java
+++ src/main/java/org/bizpay/controller/TransactionRestController.java
@@ -1,11 +1,15 @@
 package org.bizpay.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.bizpay.common.domain.InqireDelingParam;
 import org.bizpay.domain.TransByDate;
 import org.bizpay.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +31,6 @@ public class TransactionRestController {
 	@RequestMapping(value = "transByDay", method = RequestMethod.POST)
 	public ResponseEntity<List<TransByDate>> transByDay(@RequestBody InqireDelingParam param) throws Exception{
 		log.info("일자별 거래내역조회");
-		return null;
+		return new ResponseEntity<>( tService.TransByDateList(param), HttpStatus.OK);
 	}
 }
