9c341fa2f65620b39b588cab8e4de1f3dbb45970
uni.swonjiny <uni.swonjiny@gmail.com>
2021-08-04 15:23:23 +0900

    20210804

--- src/main/java/org/bizpay/common/util/SmsUtil.java
+++ src/main/java/org/bizpay/common/util/SmsUtil.java
@@ -2,11 +2,14 @@ package org.bizpay.common.util;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.apache.http.HttpResponse;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Iterator;
-
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
@@ -15,15 +18,14 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.HttpClients;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-
-
 
 import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 @Component
 @PropertySource("classpath:sms.properties")
@@ -39,8 +41,99 @@ public class SmsUtil {
 	
 	@Value("${sms.sender}")
 	private String sender;
+	
+	@Value("${naver.short.clientId}")
+	private String shortClientId;
+	
+	@Value("${naver.short.clientSecret}")
+	private String shortClientSecret;
+	
+	@Value("${naver.short.url}")
+	private String shortUrl;
+	
+	// short url 획득
+	public String getShortUrl(String apiUrl) {
+		try {
+			Map<String, String> requestHeaders = new HashMap<>();
+			requestHeaders.put("X-Naver-Client-Id", shortClientId);
+	        requestHeaders.put("X-Naver-Client-Secret", shortClientSecret);
+	        
+			URL url = new URL(shortUrl+apiUrl);
+			HttpURLConnection con =  (HttpURLConnection)url.openConnection();
+			for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+	        
+			int responseCode = con.getResponseCode();
+			
+			if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+				InputStreamReader streamReader = new InputStreamReader(con.getInputStream());
+				BufferedReader lineReader = new BufferedReader(streamReader);
+				StringBuilder responseBody = new StringBuilder();
+				String line;
+		        while ((line = lineReader.readLine()) != null) {
+		        	responseBody.append(line);
+		        }
+		 
+		        JsonElement je = JsonParser.parseString( responseBody.toString() ) ;
+		        return je.getAsJsonObject().get("result").getAsJsonObject().get("url").toString();
+            } 
+		} catch (Exception e) {
+			// 에러나면 그냥 없음
+			System.err.println(e);
+			return "";
+		}
+		return apiUrl;
 
- 
+	}
+
+	public boolean sendShortSms(String receiverNumber , String title, String msg , String receiverName ) {
+		try {
+			final String encodingType = "utf-8";
+			final String boundary = "____boundary____";
+			Gson gson=new Gson();
+			HashMap<String, String> sms = new HashMap<String, String>();
+			System.out.println("smsIdsmsId : " + smsId);
+			sms.put("user_id", smsId); // SMS 아이디
+			sms.put("key", key); //인증키
+			sms.put("msg", msg); // 메세지 내용
+			sms.put("title", title); //  제목
+			sms.put("receiver", receiverNumber); // 수신번호
+			sms.put("destination", receiverNumber+"|"+receiverName); // 수신인 %고객명% 치환
+			sms.put("msg_type", "SMS");
+			sms.put("sender", sender); // 발신번호
+			MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+			builder.setBoundary(boundary);
+			builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+			builder.setCharset(Charset.forName(encodingType));
+			for(Iterator<String> i = sms.keySet().iterator(); i.hasNext();){
+				String key = i.next();
+				builder.addTextBody(key, sms.get(key)
+						, ContentType.create("Multipart/related", encodingType));
+			}
+			HttpEntity entity = builder.build();
+			HttpClient client = HttpClients.createDefault();
+			HttpPost post = new HttpPost(sms_url);
+			post.setEntity(entity);
+			HttpResponse res = client.execute(post);
+			String result = "";
+			if(res != null){
+				BufferedReader in = new BufferedReader(new InputStreamReader(res.getEntity().getContent(), encodingType));
+				String buffer = null;
+				while((buffer = in.readLine())!=null){
+					result += buffer;
+				}
+				in.close();
+			}
+			HashMap<String,Object> dataMap=gson.fromJson(result,HashMap.class);
+			System.out.println(dataMap.toString());
+			if("1".equals(dataMap.get("result_code"))) return true;
+			else return false;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 	
 	// 단문만 1명만이다.
 	public boolean sendShortSms(String receiverNumber ,  String msg , String receiverName ) {
@@ -53,19 +146,11 @@ public class SmsUtil {
 			sms.put("user_id", smsId); // SMS 아이디
 			//sms.put("user_id", "unicore2020"); // SMS 아이디
 			sms.put("key", key); //인증키
-			//sms.put("key", "4dwg8f36zlj7uyu2jio6o7jhz2b5pwei"); //인증키
 			sms.put("msg", msg); // 메세지 내용
 			sms.put("receiver", receiverNumber); // 수신번호
 			sms.put("destination", receiverNumber+"|"+receiverName); // 수신인 %고객명% 치환
 			sms.put("msg_type", "SMS");
-			//sms.put("destination", "01039977736|송원진"); // 수신인 %고객명% 치환
 			sms.put("sender", sender); // 발신번호
-			//sms.put("title", "16000174"); // 발신번호
-			//sms.put("sender", "16000174"); // 발신번호
-			//sms.put("rdate", ""); // 예약일자 - 20161004 : 2016-10-04일기준
-			//sms.put("rtime", ""); // 예약시간 - 1930 : 오후 7시30분
-			//sms.put("testmode_yn", "N"); // Y 인경우 실제문자 전송X , 자동취소(환불) 처리
-			//sms.put("title", ""); //  LMS, MMS 제목 (미입력시 본문중 44Byte 또는 엔터 구분자 첫라인)
 			MultipartEntityBuilder builder = MultipartEntityBuilder.create();
 			builder.setBoundary(boundary);
 			builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
--- src/test/java/org/bizpay/PTest.java
+++ src/test/java/org/bizpay/PTest.java
@@ -47,78 +47,9 @@ public class PTest {
 	
 	@Test
 	void mainTest() throws Exception {
-		String nameList = "상품1#상품2#상품3";
-		ArrayList<String> arrNameList = new ArrayList<String>();
-	for (String item : nameList.split("#")) {
-		arrNameList.add(item);
-		}
-		System.out.println(arrNameList.toString());
 		
-		
-//		Hashtable xht = ksBean.sendCardCancelMsg(
-//				KSNET_PG_IP, 				// -필수- ipaddr  X(15)   *KSNET_IP(개발:210.181.28.116, 운영:210.181.28.137)  
-//				KSNET_PG_PORT,			// -필수- port   9( 5)   *KSNET_PORT(21001)  
-//				"2552500002", 								// -필수- pStoreId  X(10)   *상점아이디(개발:2999199999, 운영:?)  
-//				"K", 							// -필수- pKeyInType   X(12)  KEY-IN유형(K:직접입력,S:리더기사용입력)  
-//				"178270066440"						// -필수- pTransactionNo  X( 1)  *거래번호(승인응답시의 KEY:1로시작되는 12자리숫자)  
-//			);
-//		
-//		System.out.println(xht.toString());
-//		ExternalOrderInputParam param = new ExternalOrderInputParam();
-//		
-//		//smsUtil.sendShortSms("01039977736", "noti 서버오류"+"\n주문번호 : A4123875157845487\n주문영:피시방A세트포인트구매", "큐알거래");
-//		
-//		String temp= "B0002A1777필수옵션 1개50060b737bf0f3ec733b804c8f7unicore";
-//		
-//		String temm = eUtil.encryptSHA256(temp);
-//		
-//		System.out.println(temm);
-				
-//		param.setConfmNo("00314439");
-//		param.setExorderNo("exS0007");
-//		param.setMberId("B0002A1777");
-//		exSever.payCancel(param);
-//		ExternalOrderInputParam exInfo = emap.selectOrderInfo2(param);
-//		System.out.println(exInfo.toString());
-		// 시전 주문 입력
-//		ExternalOrderInputParam param = new ExternalOrderInputParam();
-//		param.setMberId("unicore");
-//		param.setOrderName("큐알테스트2-쫄면");
-//		param.setOrderPrice(8120);
-//		
-//		System.out.println("***********************입력전*******************************************");
-//		System.out.println(param.toString());
-//		exSever.insertExOrder(param);
-//		System.out.println("***********************입력후*****************************************");
-//		System.out.println(param.toString()); // 21 // 22
-		
-		// 이력후 주문 정보추출
-//		ExternalOrderInfo info1 = exSever.selectOrderInfo(22);
-//		System.out.println(info1.toString());
-		
-		// 수기결제 요청
-		
-//		PaymentReqParam ppp = new PaymentReqParam();
-//		ppp.setAmount(500);
-//		ppp.setCardNo("6243760025280317");
-//		ppp.setGoodsName("큐알테스트4쫄면");
-//		ppp.setInstallment("00");
-//		ppp.setMemberId("B0002A1777"); // 유니코아 송원진
-//		ppp.setEmail("aaa@bbb.com");
-//		ppp.setPhoneNumber("01039977736");
-//		ppp.setExpiration("2507");
-//		ppp.setPasswd("99");
-//		ppp.setPidNum("810204");// 주민번호
-//		exSever.payRequest(ppp);
-		
-//		String aaaa = eUtil.encryptSHA256("상품테스트5000");
-//		System.out.println(aaaa);
-		//ExternalOrderInputParam param = new ExternalOrderInputParam();
-		
-//		exSever.payCancel(param);
-//		boolean falf = smsUtil.sendShortSms("01039977736", "테스트중", "이름");
-//		System.out.println(falf);
-		
-		//smsUtil.mailSender();
+		String str = smsUtil.getShortUrl("https://play.google.com/store/apps/details?id=kr.co.linkapp.swipe.bizpay");
+		System.out.println(str);
+	
 	}
 }
